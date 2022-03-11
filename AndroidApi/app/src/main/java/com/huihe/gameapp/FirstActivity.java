23e2f1ee9cb27aeaffa8be448364a99382b2e3c2
linsan <xx@xx.xx>
2022-03-11 12:18:58 +0800

    V1
    1. reset gradle
    2. fixed add custom activity (Not found R.layout_id)
    3. run sucessful with out aar/classes.jar

--- AndroidApi/app/src/main/java/com/huihe/gameapp/FirstActivity.java
+++ AndroidApi/app/src/main/java/com/huihe/gameapp/FirstActivity.java
@@ -3,11 +3,11 @@ package com.huihe.gameapp;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Toast;
-
 
 import com.hjq.permissions.OnPermissionCallback;
 import com.hjq.permissions.OnPermissionPageCallback;
@@ -22,7 +22,10 @@ public class FirstActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first);
+        // setContentView(R.layout.activity_first);
+
+        int layoutId = getResources().getIdentifier("activity_first", "layout", getPackageName());
+        setContentView(layoutId);
 
 
         checkPermissions();
@@ -67,32 +70,48 @@ public class FirstActivity extends Activity {
     private static final int REQUEST_CODE_SETTING = 0x0012345;
 
     @TargetApi(Build.VERSION_CODES.M)
-    private void showPermissionDeniedDialog(List<String> permissions) {
+    private void showPermissionDeniedDialog( List<String> permissions) {
+
+        final Activity self = this;
+        final List<String> ppps = permissions;
+
         //启动当前App的系统设置界面
         AlertDialog dialog = new AlertDialog.Builder(this)
                 .setTitle("帮助")
                 .setMessage("当前应用缺少必要权限")
-                .setNegativeButton("取消", (dialog12, which) -> dialog12.dismiss())
-                .setPositiveButton("设置", (dialog1, which) -> {
-                    // 如果是被永久拒绝就跳转到应用权限系统设置页面
-                    XXPermissions.startPermissionActivity(this, permissions, new OnPermissionPageCallback() {
-                        @Override
-                        public void onGranted() {
-                            startGame();
-                        }
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .
+                        setPositiveButton("设置", new  DialogInterface.OnClickListener() {
 
-                        @Override
-                        public void onDenied() {
-                            Toast.makeText(FirstActivity.this, "被永久拒绝授权后无法进入游戏，请重启应用后手动授予.", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }).create();
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // 如果是被永久拒绝就跳转到应用权限系统设置页面
+                                XXPermissions.startPermissionActivity(self, ppps, new OnPermissionPageCallback() {
+                                    @Override
+                                    public void onGranted() {
+                                        startGame();
+                                    }
+
+                                    @Override
+                                    public void onDenied() {
+                                        Toast.makeText(FirstActivity.this, "被永久拒绝授权后无法进入游戏，请重启应用后手动授予.", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        }).create();
         dialog.show();
     }
+
 
     private void startGame() {
         Intent intent = new Intent(this, GameMainActivity.class);
         startActivity(intent);
+        finish();
     }
 
 }
